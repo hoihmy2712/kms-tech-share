45a71e5ffb3cccede100412e047f42ef970d1fe7
myhoih <my.hoih@student.passerellesnumeriques.org>
2023-03-08 22:31:27 +0700

    Adding react app

---
+++ react-js-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import AppRouter from './component/RouterComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
